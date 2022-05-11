3a5e7c3371c65ba112212d5884ca473c3afb7a4c
omar nazih <omarnazeh295@gmail.com>
2022-05-11 23:44:55 +0200

    updated req, and db connections

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
+import sqlalchemy
 from flask import Flask
 from app import env
-import pymysql
 
 app = Flask(__name__)
 
@@ -16,38 +16,23 @@ app.config['UPLOAD_FOLDER']='app/uploader/'
 app.config['TEMPLATES']='app/templates/'
 app.config['UPLOAD_IMAGE']='app/static/img/'
 app.config['MAX_CONTENT_PATH']=5*1024*1024
-
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-# DataBase connection
 def init_connection_engine():
-
-  connection = pymysql.connect(
-    host=os.environ.get('MYSQL_HOST'),
-    user=os.environ.get('MYSQL_USER'),
-    password=os.environ.get('MYSQL_PASSWORD'),
-    db=os.environ.get('MYSQL_DB'),
-    cursorclass=pymysql.cursors.DictCursor
-  )
-  
-  # Return connection
-  return connection
-# def init_connection_engine():
-#     pool = sqlalchemy.create_engine(
-#         sqlalchemy.engine.url.URL(
-#             drivername="mysql",
-#             username=os.environ.get('MYSQL_USER'), #username
-#             password=os.environ.get('MYSQL_PASSWORD'), #password
-#             database=os.environ.get('MYSQL_DB'), #database name
-#             host=os.environ.get('MYSQL_HOST') #ip
-#         )
-#     )
-
-#     return pool
+    pool = sqlalchemy.create_engine(
+        sqlalchemy.engine.url.URL(
+            drivername="mysql",
+            username=os.environ.get('MYSQL_USER'), #username
+            password=os.environ.get('MYSQL_PASSWORD'), #password
+            database=os.environ.get('MYSQL_DB'), #database name
+            host=os.environ.get('MYSQL_HOST') #ip
+        )
+    )
+    return pool
 
 # bcrypt = Bcrypt()
 # Cursor
-con = init_connection_engine()
+db = init_connection_engine()
 
 # bcrypt.init_app(app)
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,18 @@
 import os
-from app import app, con
+from app import app, db
 from flask import Flask, jsonify,session, render_template, request, redirect, url_for,flash,make_response, Response,send_from_directory
 from datetime import datetime
 
 def fetchQuery(query):
-   con.begin()
-
-   cursor = con.cursor()
-   cursor.execute(query);
-   res = cursor.fetchone()
-
-   con.close()   
-
-   return res
-   
-
+   conn = db.connect()             
+   sql = query
+   result = conn.execute(sql).fetchall()  
+   print(result)
+   return result
 # Default route is home page
 @app.route('/')
 
 @app.route('/home')
 def home_page():  
-
-   res = fetchQuery("select * from system_users")
+   res = fetchQuery("select * from system_users")   
    return render_template('login.html', title = "home page", res=res)

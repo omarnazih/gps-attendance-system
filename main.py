1dbd912783d1b5ca98ced85875276ec90d69a8a2
omar nazih <omarnazeh295@gmail.com>
2022-06-10 18:30:46 +0200

    final update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from app import app
 
 # HTTPS (Self Signed Cert)
-# if __name__ == "__main__":  
-#     app.run(host='0.0.0.0',ssl_context=("cert.pem", "key.pem"))
+if __name__ == "__main__":  
+    app.run(host='0.0.0.0',ssl_context=("cert.pem", "key.pem"))
 
 # HTTP 
-if __name__ == '__main__':
-    app.run(debug = True,port=5000)  
+# if __name__ == '__main__':
+#     app.run(debug = True,port=5000)  

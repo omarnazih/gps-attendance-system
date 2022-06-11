8747b9eaec4b8e3524321d2f8631483761c2df47
omar nazih <omarnazeh295@gmail.com>
2022-06-11 19:42:10 +0200

    browser loading fix

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from app import app
 
 # HTTPS (Self Signed Cert)
-if __name__ == "__main__":  
+if __name__ == "__main__":
+    # To make sure site loads on HTTPS://
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True      
     app.run(host='0.0.0.0',ssl_context=("cert.pem", "key.pem"))
 
 # HTTP 
